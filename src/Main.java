4d4b9e0c1c236cbd58d136ffc9497d0c287c28ec
Dimitrios Vlastaras <mail@dimme.net>
2013-03-18 14:40:52 +0100

    Any last changes

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,17 @@
+import javax.swing.UIManager;
+
+/**
+ * Author: Dimitrios Vlastaras
+ * Date..: 2013-03-13
+ * Time..: 17:41
+ */
 
 public class Main {
 
     public static void main(String[] args) {
-
-        MainWindow window = new MainWindow();
-
-
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception e) {}
+        new MainWindow();
     }
 }
